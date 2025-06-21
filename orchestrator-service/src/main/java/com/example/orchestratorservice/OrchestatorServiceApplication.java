e47f1d0239e29c2cf611c10062b212051d302a2d
MaciejKiec <harmonicisland8@gmail.com>
2025-06-21 22:33:42 +0200

    feat: added common module package scanning to payment service and orchestrator service

--- orchestrator-service/src/main/java/com/example/orchestratorservice/OrchestatorServiceApplication.java
+++ orchestrator-service/src/main/java/com/example/orchestratorservice/OrchestatorServiceApplication.java
@@ -1,11 +1,14 @@
 package com.example.orchestratorservice;
 
+import org.common.config.EventBusAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@Import(EventBusAutoConfiguration.class)
 public class OrchestatorServiceApplication {
 
     public static void main(String[] args) {
--- payment-service/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/example/paymentservice/PaymentServiceApplication.java
@@ -1,13 +1,18 @@
 package com.example.paymentservice;
 
+import org.common.config.EventBusAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableScheduling
+@Import({EventBusAutoConfiguration.class})
 public class PaymentServiceApplication {
 
     public static void main(String[] args) {
