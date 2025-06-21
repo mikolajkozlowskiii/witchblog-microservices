376aec0aa5dc061069ef92586780c5d0bd2c127d
MaciejKiec <harmonicisland8@gmail.com>
2025-06-21 22:23:19 +0200

    feat: removed unnecessary classess

--- common-module/src/main/java/org/common/event/PaymentSessionCreatedEvent.java
+++
@@ -1,2 +0,0 @@
-package org.common.event;public class PaymentSessionCreatedEvent {
-}
--- orchestrator-service/src/main/java/com/example/orchestratorservice/repository/JobRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.orchestratorservice.repository;
-
-import com.example.orchestratorservice.model.Job;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<Job, String> {}
-
