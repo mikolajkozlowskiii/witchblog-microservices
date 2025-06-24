0caa968c5bb399b38265455439560796bed4e26c
MaciejKiec <harmonicisland8@gmail.com>
2025-06-24 16:23:10 +0200

    feat: added management service

---
+++ management-service/src/main/java/com/example/ManagementServiceApplication.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.common.config.EventBusAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Import;
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
+@Import({EventBusAutoConfiguration.class})
+public class ManagementServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementServiceApplication.class, args);
+    }
+}
